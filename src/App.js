22d5a8fde5d78dd5a561ae7373d6612d1ac85b21
zan69 <marcellopz8@gmail.com>
2022-06-28 12:22:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Counter from './components/Counter';
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+import UserProfile from "./components/UserProfile";
+
+
+function App() {
+    const isAuth = useSelector(state => state.auth.isAuthenticated)
+    console.log(isAuth)
+    return (
+        <>
+            <Header/>
+            {!isAuth && <Auth/>}
+            {isAuth && <UserProfile/>}
+            <Counter/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import {useDispatch, useSelector} from "react-redux";
+import {counterActions} from "../store/counter-slice";
+
+import classes from './Counter.module.css';
+
+const Counter = () => {
+    const counter = useSelector(state => state.counter.counter);
+    const show = useSelector(state => state.counter.showCounter);
+    const dispatch = useDispatch();
+
+    const incrementHandler = () => {
+        dispatch(counterActions.increment(''))
+    }
+    const incrementHandler5 = () => {
+        dispatch(counterActions.increase(5))
+    }
+    const decrementHandler = () => {
+        dispatch(counterActions.decrement(''))
+    }
+    const toggleCounterHandler = () => {
+        dispatch(counterActions.toggleCounter(''))
+    }
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            {show && <div className={classes.value}>{counter}</div>}
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={incrementHandler5}>Increment by 5</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
+};
+
+export default Counter;
